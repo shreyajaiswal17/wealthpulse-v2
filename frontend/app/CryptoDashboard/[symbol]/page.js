2a7471088e76397cf1ab78fae856e09428637fe8
codedpool <codedpool10@gmail.com>
2026-03-21 15:47:23 +0530

    crypto fix

--- frontend/app/CryptoDashboard/[symbol]/page.js
+++ frontend/app/CryptoDashboard/[symbol]/page.js
@@ -1,7 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
-import { useParams } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 import {
   LineChart,
   Line,
